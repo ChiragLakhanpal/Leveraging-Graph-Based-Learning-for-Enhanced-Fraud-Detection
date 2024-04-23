83583e98ec762c708eaef8a1d8a349d1f353bc84
sunishaharish <sunisha.harish@gmail.com>
2024-04-23 01:21:53 -0400

    Added Data Loader code

--- code/component/CNN.py
+++ code/component/CNN.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, \
     average_precision_score, confusion_matrix, precision_recall_curve, roc_curve
 import matplotlib.pyplot as plt
@@ -18,49 +19,49 @@ import prettytable
 seed = 42
 np.random.seed(seed)
 
-torch.manual_seed(42)
+torch.manual_seed(seed)
 if torch.cuda.is_available():
-    torch.cuda.manual_seed(42)
+    torch.cuda.manual_seed(seed)
 
-data = pl.read_csv('/home/ec2-user/DS_Capstone/Data/Sampled Dataset.csv')
+def dataset_stats(data, X_train, X_test, y_train, y_test):
+    table = prettytable.PrettyTable()
 
-data = preprocess_data(data)
+    # Display dataset stats
+    print("Dataset Stats")
 
-data = data.to_pandas()
+    table.field_names = ["Data", "Rows", "Columns", "Frauds", "Non-Frauds", "Fraud Percentage"]
+    table.add_row(
+        ["Complete Dataset", data.shape[0], data.shape[1], data['is_fraud'].sum(), data.shape[0] - data['is_fraud'].sum(),
+         f"{round(data['is_fraud'].sum() / data.shape[0] * 100, 2)}%"])
+    table.add_row(["Train", X_train.shape[0], X_train.shape[1], y_train.sum(), y_train.shape[0] - y_train.sum(),
+                   f"{round(y_train.sum() / y_train.shape[0] * 100, 2)}%"])
+    table.add_row(["Test", X_test.shape[0], X_test.shape[1], y_test.sum(), y_test.shape[0] - y_test.sum(),
+                   f"{round(y_test.sum() / y_test.shape[0] * 100, 2)}%"])
+    print(table)
+class CustomDataset(Dataset):
+    def __init__(self,X,y):
+        self.X = torch.tensor(X, dtype=torch.float32, device=device)
+        self.y = torch.tensor(y, dtype=torch.float32, device=device)
+    def __len__(self):
+        return self.X.size(0)
+    def __getitem__(self,idx):
+        return self.X[idx], self.y[idx]
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class CustomDataLoader:
+    def __init__(self,  batch_size=128):
+        self.batch_size = batch_size
+    def prepare_train_val_loader(self, X_train, y_train, X_test, y_test):
 
-X = data.drop('is_fraud', axis=1)
-y = data['is_fraud']
+        train_dataset = CustomDataset(X_train,y_train)
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True,num_workers=8)
+        val_dataset = CustomDataset(X_test,y_test)
+        val_loader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False,num_workers=8)
+        return train_loader, val_loader
+    def prepare_test_dev_loader(self, test_data):
 
-X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.2)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-table = prettytable.PrettyTable()
-
-# Display dataset stats
-print("Dataset Stats")
-
-table.field_names = ["Data", "Rows", "Columns", "Frauds", "Non-Frauds", "Fraud Percentage"]
-table.add_row(
-    ["Complete Dataset", data.shape[0], data.shape[1], data['is_fraud'].sum(), data.shape[0] - data['is_fraud'].sum(),
-     f"{round(data['is_fraud'].sum() / data.shape[0] * 100, 2)}%"])
-table.add_row(["Train", X_train.shape[0], X_train.shape[1], y_train.sum(), y_train.shape[0] - y_train.sum(),
-               f"{round(y_train.sum() / y_train.shape[0] * 100, 2)}%"])
-table.add_row(["Test", X_test.shape[0], X_test.shape[1], y_test.sum(), y_test.shape[0] - y_test.sum(),
-               f"{round(y_test.sum() / y_test.shape[0] * 100, 2)}%"])
-print(table)
-
-X_train = torch.tensor(X_train, dtype=torch.float32, device=device)
-X_test = torch.tensor(X_test, dtype=torch.float32, device=device)
-y_train = torch.tensor(y_train.values, dtype=torch.float32, device=device)
-y_test = torch.tensor(y_test.values, dtype=torch.float32, device=device)
-
-X_train = X_train.unsqueeze(1)
-X_test = X_test.unsqueeze(1)
+        test_dataset = CustomDataset(test_data)
+        test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+        return test_loader
 
 class CNN(nn.Module):
     def __init__(self, input_dim, output_dim):
@@ -86,15 +87,137 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return F.sigmoid(x)
 
+def train_val(train_loader, val_Loader, epochs):
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0
+        for inputs,targets in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets.unsqueeze(1))
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+
+        # Validation
+        model.eval()
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                val_outputs = model(inputs)
+                val_loss = criterion(val_outputs, target.unsqueeze(1)).item()
+                y_pred = (val_outputs > 0.5).float()
+                y_true = y_test.unsqueeze(1)
+
+                precision = precision_score(y_true.cpu(), y_pred.cpu())
+                recall = recall_score(y_true.cpu(), y_pred.cpu())
+                f1 = f1_score(y_true.cpu(), y_pred.cpu())
+
+                trial_log.append({
+                    'epoch': epoch + 1,
+                    'train_loss': total_loss / (X_train.shape[0] // batch_size),
+                    'val_loss': val_loss,
+                    'precision': precision,
+                    'recall': recall,
+                    'f1': f1,
+                })
+
+                train_losses.append(total_loss / (X_train.shape[0] // batch_size))
+                val_losses.append(val_loss)
+                f1_scores.append(f1)
+
+                if f1 > best_val_f1:
+                    best_val_f1 = f1
+                    best_model = model.state_dict()
+                    print('Model updated!')
+
+                print(
+                    f"Epoch {epoch + 1}/{epochs}, Train Loss: {total_loss:.4f}, Val Loss: {val_loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}")
+
+        model.load_state_dict(best_model)
+
+        # Evaluation
+        model.eval()
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                test_outputs = model(inputs)
+                y_pred = (test_outputs > 0.5).float().cpu().numpy()
+                y_true = target.cpu().numpy()
+
+        # Metrics
+        conf_matrix = confusion_matrix(y_true, y_pred)
+        accuracy = accuracy_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        roc_auc = roc_auc_score(y_true, y_pred)
+        aucpr = average_precision_score(y_true, y_pred)
+
+        # Print metrics
+        results = prettytable.PrettyTable(title='CNN Results')
+        results.field_names = ["Metric", "Value"]
+        results.add_row(["Accuracy", accuracy])
+        results.add_row(["Precision", precision])
+        results.add_row(["Recall", recall])
+        results.add_row(["F1 Score", f1])
+        results.add_row(["ROC AUC", roc_auc])
+        results.add_row(["AUCPR", aucpr])
+        print(results)
+
+plt.figure(figsize=(12, 5))
+
+def loss_plot():
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, epochs + 1), train_losses, 'b-', label='Training Loss')
+    plt.plot(range(1, epochs + 1), val_losses, 'r-', label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training & Validation Loss')
+    plt.legend(loc='best')
+
+def f1_score_plot():
+    plt.subplot(1, 2, 2)
+    plt.plot(range(1, epochs + 1), f1_scores, 'g-', label='F1 Score')
+    plt.xlabel('Epoch')
+    plt.ylabel('F1 Score')
+    plt.title('F1 Score over Epochs')
+    plt.legend(loc='best')
+
+def confusion_matrix():
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt=',d', cmap='Blues')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title('Confusion Matrix')
+
+def save_roc_curve():
+    fpr, tpr, thresholds = roc_curve(y_true, y_pred)
+    plt.figure(figsize=(8, 8))
+    plt.plot(fpr, tpr, marker='.', label=f'ROC curve (area = {roc_auc:.2f})')
+    plt.plot([0, 1], [0, 1], linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend()
+
+def save_pr_curve():
+    prc, rec, _ = precision_recall_curve(y_true, y_pred)
+    plt.figure(figsize=(8, 8))
+    plt.plot(recall_curve, precision_curve, marker='.', label=f'Precision-Recall curve (area = {aucpr:.2f})')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curve')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
 input_dim = 28
 output_dim = 1
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = CNN(input_dim, output_dim).to(device)
 criterion = nn.BCELoss()
-
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-epochs = 50
 batch_size = 128
 best_val_loss = np.inf
 best_val_f1 = 0.0
@@ -104,127 +227,40 @@ train_losses = []
 val_losses = []
 f1_scores = []
 
-# Training 
-for epoch in range(epochs):
-    model.train()
-    total_loss = 0
-    for i in range(0, X_train.shape[0], batch_size):
-        inputs = X_train[i:i + batch_size]
-        targets = y_train[i:i + batch_size]
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets.unsqueeze(1))
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    #scheduler.step(f1)
 
-    # Validation
-    model.eval()
-    with torch.no_grad():
-        val_outputs = model(X_test)
-        val_loss = criterion(val_outputs, y_test.unsqueeze(1)).item()
-        y_pred = (val_outputs > 0.5).float()
-        y_true = y_test.unsqueeze(1)
+def main():
 
-        precision = precision_score(y_true.cpu(), y_pred.cpu())
-        recall = recall_score(y_true.cpu(), y_pred.cpu())
-        f1 = f1_score(y_true.cpu(), y_pred.cpu())
+    data = pl.read_csv('/home/ec2-user/DS_Capstone/Data/Sampled Dataset.csv')
 
-        trial_log.append({
-            'epoch': epoch + 1,
-            'train_loss': total_loss / (X_train.shape[0] // batch_size),
-            'val_loss': val_loss,
-            'precision': precision,
-            'recall': recall,
-            'f1': f1,
-        })
+    data = preprocess_data(data)
 
-        train_losses.append(total_loss / (X_train.shape[0] // batch_size))
-        val_losses.append(val_loss)
-        f1_scores.append(f1)
+    data = data.to_pandas()
 
-        if f1 > best_val_f1:
-            best_val_f1 = f1
-            best_model = model.state_dict()
-            print('Model updated!')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        print(
-            f"Epoch {epoch + 1}/{epochs}, Train Loss: {total_loss:.4f}, Val Loss: {val_loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}")
+    X = data.drop('is_fraud', axis=1)
+    y = data['is_fraud']
 
-model.load_state_dict(best_model)
+    X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.2)
 
-# Evaluation 
-model.eval()
-with torch.no_grad():
-    test_outputs = model(X_test)
-    y_pred = (test_outputs > 0.5).float().cpu().numpy()
-    y_true = y_test.cpu().numpy()
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
-# Metrics
-conf_matrix = confusion_matrix(y_true, y_pred)
-accuracy = accuracy_score(y_true, y_pred)
-precision = precision_score(y_true, y_pred)
-recall = recall_score(y_true, y_pred)
-f1 = f1_score(y_true, y_pred)
-roc_auc = roc_auc_score(y_true, y_pred)
-aucpr = average_precision_score(y_true, y_pred)
+    X_train = torch.tensor(X_train, dtype=torch.float32, device=device)
+    X_test = torch.tensor(X_test, dtype=torch.float32, device=device)
+    y_train = torch.tensor(y_train.values, dtype=torch.float32, device=device)
+    y_test = torch.tensor(y_test.values, dtype=torch.float32, device=device)
 
-# Print metrics
-results = prettytable.PrettyTable(title='CNN Results')
-results.field_names = ["Metric", "Value"]
-results.add_row(["Accuracy", accuracy])
-results.add_row(["Precision", precision])
-results.add_row(["Recall", recall])
-results.add_row(["F1 Score", f1])
-results.add_row(["ROC AUC", roc_auc])
-results.add_row(["AUCPR", aucpr])
-print(results)
+    X_train = X_train.unsqueeze(1)
+    X_test = X_test.unsqueeze(1)
+    
+    epochs = 50
 
-plt.figure(figsize=(12, 5))
+    data_loader = CustomDataLoader()
+    train_loader, val_loader = data_loader.prepare_train_val_loader(X_train, y_train, X_test, y_test)
 
-# Plot Losses
-plt.subplot(1, 2, 1)
-plt.plot(range(1, epochs + 1), train_losses, 'b-', label='Training Loss')
-plt.plot(range(1, epochs + 1), val_losses, 'r-', label='Validation Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Training & Validation Loss')
-plt.legend(loc='best')
+    train_val(train_loader, val_loader, epochs)
 
-# Plot F1 Score
-plt.subplot(1, 2, 2)
-plt.plot(range(1, epochs + 1), f1_scores, 'g-', label='F1 Score')
-plt.xlabel('Epoch')
-plt.ylabel('F1 Score')
-plt.title('F1 Score over Epochs')
-plt.legend(loc='best')
-
-# Plot the confusion matrix
-plt.figure(figsize=(8, 6))
-sns.heatmap(conf_matrix, annot=True, fmt=',d', cmap='Blues')
-plt.xlabel('Predicted labels')
-plt.ylabel('True labels')
-plt.title('Confusion Matrix')
-
-# Visualize the ROC curve
-fpr, tpr, thresholds = roc_curve(y_true, y_pred)
-plt.figure(figsize=(8, 8))
-plt.plot(fpr, tpr, marker='.', label=f'ROC curve (area = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], linestyle='--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC Curve')
-plt.legend()
-
-# Visualize the Precision-Recall curve
-precision_curve, recall_curve, _ = precision_recall_curve(y_true, y_pred)
-plt.figure(figsize=(8, 8))
-plt.plot(recall_curve, precision_curve, marker='.', label=f'Precision-Recall curve (area = {aucpr:.2f})')
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.title('Precision-Recall Curve')
-plt.legend()
-
-plt.tight_layout()
-plt.show()
+if __name__ == "__main__":
+    main()
